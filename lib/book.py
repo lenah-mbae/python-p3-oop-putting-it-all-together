fd4ddf48a1cf79dc1fc4b29f8e3c7240664c4e3e
Lenah Mbae <lenahmbae22@gmail.com>
2025-05-16 17:19:14 +0300

    tests

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        """"The title property"""
+        return self._page_count
     
-        
+    @page_count.setter
+    def page_count(self, page_count):
+        """"Page_count must be an interger"""
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    @property
+    def turn_page(self):
+        """"The turn_page property"""
+        return self._turn_page
+    
+   
+    def turn_page(self):
+       
+            print("Flipping the page...wow, you read fast!")
+
